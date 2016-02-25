959fa37b3c8189591c3969f1539b8aecf600e677
Guilherme Morais <gtmorais@gmail.com>
2016-02-25 11:24:02 -0300

    Added files via upload

---
+++ MVC_ClientWCFAsync/MVC_ClientWCF/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Proxies;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+//using MyServiceProxy;
+
+namespace MVC_ClientWCF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            await ChamarServico();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            Chamador();
+
+            return View();
+        }
+
+
+        private static async Task<RetornoContract> ChamarServico()
+        {
+            using (HistoricoFluxoServiceClient client = new HistoricoFluxoServiceClient())
+            {
+                return await client.IncluirHistoricoFluxoTaskAsync("autorizacao", new HistoricoAutorizacaoContract()
+                {
+                    Acao = "teste",
+                    Criado = DateTime.Now,
+                    Historico = "historico",
+                    IDItemCriado = 1,
+                    ListaItemOrigem = "origem"
+                }, false);
+            }
+        }
+
+
+        private Task<RetornoContract> Chamador()
+        {
+            return Task.Run(new Func<Task<RetornoContract>>(async () =>
+            {
+                using (HistoricoFluxoServiceClient client = new HistoricoFluxoServiceClient())
+                {
+                    return await client.IncluirHistoricoFluxoTaskAsync("autorizacao", new HistoricoAutorizacaoContract()
+                    {
+                        Acao = "teste",
+                        Criado = DateTime.Now,
+                        Historico = "historico",
+                        IDItemCriado = 1,
+                        ListaItemOrigem = "origem"
+                    }, false);
+                }
+            }));
+        }
+
+
+        private static async void ChamadorAsync()
+        {
+            using (HistoricoFluxoServiceClient client = new HistoricoFluxoServiceClient())
+            {
+                await Task.Factory.StartNew(() =>
+                {
+                    client.IncluirHistoricoFluxoTaskAsync("autorizacao", new HistoricoAutorizacaoContract()
+                    {
+                        Acao = "teste",
+                        Criado = DateTime.Now,
+                        Historico = "historico",
+                        IDItemCriado = 1,
+                        ListaItemOrigem = "origem"
+                    }, false);
+                });
+            }
+        }
+    }
+}
---
+++ MVC_ClientWCFAsync/MVC_ClientWCF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_ClientWCF.Startup))]
+namespace MVC_ClientWCF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
